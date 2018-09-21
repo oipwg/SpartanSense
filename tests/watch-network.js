37c9ade130e621c3e83a72bff1e1d0a92be803b6
Sky Young <sky@alexandria.io>
2018-09-21 12:16:25 -0700

    use livenet

--- tests/watch-network.js
+++ tests/watch-network.js
@@ -3,5 +3,5 @@ var ChainScanner = require("../lib/ChainScanner").default
 var scanner = new ChainScanner({
 	log_level: "debug",
 	peer_log_level: "debug",
-	network: "flotestnet"
+	network: "flolivenet"
 })
